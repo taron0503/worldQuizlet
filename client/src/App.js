9c5537df80b896004f4c1e4001ff15dafebd940a
taron0503 <taron0503@gmail.com>
2022-09-02 17:25:11 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Board from './components/Board/board';
+
+function App() {
+  return (
+    <div className="App">
+      <Board></Board>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Board/board.js
@@ -0,0 +1,12 @@
+import { Component } from "react"
+import "./style.css"
+
+export default class Board extends Component{
+    render(){
+        return <div className="Board">
+            <header className="Board-header">
+                WorldQuizlet
+            </header>
+        </div>
+    }
+}
